ee04573fd0baaa3a92036b09883ffe201bc16cef
eugmani <eugmani@gmail.com>
2021-06-29 18:42:10 +0200

    first commit

---
+++ src/test/java/com/eu/microservicios/MicroserviciosEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eu.microservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
